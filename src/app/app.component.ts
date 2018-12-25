53d288d052276f1f80885995f05869d829394d8c
oniipon <oniipon@gmail.com>
2018-12-25 16:27:36 +0900

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'datatble';
+  rows = [];
+  c = 'id';
+  t: any;
+  ngOnInit(): void {
+    const tmp = [];
+    for (let i = 0; i < 500; i++) {
+      console.log(i);
+      tmp[i] = { id: i, name: `田中${i}`, height: Math.floor(Math.random() * 80) + 50 };
+    }
+    this.rows = tmp;
+  }
+
+  getRowHeight(row) {
+    return row.height;
+  }
 }
